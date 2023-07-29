5f64e9ac90936a2de75c8a4aa7efce48b6a51762
neuthos <ardiangalang05@gmail.com>
2023-07-29 18:31:38 +0800

    menambahkan day-1 dan day-2

---
+++ notes.js
@@ -0,0 +1,17 @@
+/**
+ * MEMBUAT REPOSITORY
+ * 1. Membuat repository
+ * 2. Input Nama repository
+ * 3. Centang "Add a README file"
+ * 4. Tekan tombol Create Reporitory 
+ * 
+ * 5. Copy Link dari repository Ex: https://github.com/neuthos/galang-sebulan-bisa-ngoding.git
+ * 6. Di Terminal Laptop eksekusi code ini "git clone {url dari repository}"
+ * 7. Folder dari github sudah dibuat dan sudah bisa ngoding
+ */
+
+/**
+ * UNTUK MENGUMPULKAN TASK
+ * 1. Buka terminal di vscode
+ * 
+ */
