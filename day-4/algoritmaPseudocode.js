63a82f587becabe2a02bfe680b48cc506b4c9898
neuthos <ardiangalang05@gmail.com>
2023-07-29 18:55:36 +0800

    menyelesaikan task algoritma dan pseudocode

---
+++ day-4/algoritmaPseudocode.js
@@ -0,0 +1,26 @@
+
+
+// JAWABAN NOMOR 3
+let modulusExample = 11 % 2
+console.log({modulusExample})
+
+/**
+ * ALGORITMA
+ * 1. Memulai program
+ * 2. Minta pengguna untuk memasukkan angka yang akan di cek
+ * 3. Jika angka yang dimasukkan di modulus dengan 2 dan hasilnya adalah 0 maka dia Genap
+ * 4. Jika tidak maka dia ganjil
+ * 5. Program Selesai
+ */
+
+/**
+ * PSEUDOCODE:
+ * START
+ *  INPUT angka
+ *  IF angka % 2 = 0 THEN
+ *    DISPLAY "Angka adalah bilangan Genap"
+ *  ELSE 
+ *    DISPLAY "Angka adalah bilangan Ganjil"
+ *  ENDIF
+ * END
+ */
